c2891a1d53e4f37f93e300facc295c4861d19318
anandvfcvijay <49161738+anandvfcvijay@users.noreply.github.com>
2019-06-03 15:02:30 +0530

    Update __init__.py

--- haruka/__init__.py
+++ haruka/__init__.py
@@ -59,6 +59,8 @@ if ENV:
     ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
     STRICT_GMUTE = bool(os.environ.get('STRICT_GMUTE', False))
     API_WEATHER = os.environ.get('API_OPENWEATHER', None)
+    DEEPFRY_TOKEN = os.environ.get('DEEPFRY_TOKEN ', None)
+
 else:
     from haruka.config import Development as Config
     TOKEN = Config.API_KEY
